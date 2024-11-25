26276193fbc4ae338bab0888d6d9c7e3f2b2d83d
Puja Wagh <co.2021.ppwagh@bitwardha.ac.in>
2024-11-25 15:49:41 +0530

    changes

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.use('/api/comments', commentRoutes); // Comment routes
 
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
